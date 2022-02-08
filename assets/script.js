bbc5fb4badb2464d3ccd918fc24446e19000c77c
Brandon Dega <brandondega@gmail.com>
2022-02-07 18:56:05 -0600

    we did it

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,77 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
+
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Brandon Dega" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let total = 0
+let gbQuantity = document.querySelector(`#qty-gb`)
+let ccQuantity = document.querySelector(`#qty-cc`)
+let sugarQuantity = document.querySelector(`#qty-sugar`)
+let tableTotal = document.querySelector(`#qty-total`)
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
 // selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
+let gbPlusBtn = document.querySelector('#add-gb')
+let gbMinusBtn = document.querySelector('#minus-gb')
+let ccPlusBtn = document.querySelector('#add-cc')
+let ccMinusBtn = document.querySelector('#minus-cc')
+let sugarPlusBtn = document.querySelector('#add-sugar')
+let sugarMinusBtn = document.querySelector('#minus-sugar')
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+    gb = gb + 1
+    total = gb + cc + sugar
+    gbQuantity.textContent = gb
+    tableTotal.textContent = total
+})
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+gbMinusBtn.addEventListener(`click`,function() {
+    if(gb > 0){
+        gb = gb - 1
+        total = gb + cc + sugar
+        gbQuantity.textContent = gb
+        tableTotal.textContent = total
+    }
+})
+
+ccPlusBtn.addEventListener('click', function() {
+    cc = cc + 1
+    total = gb + cc + sugar
+    ccQuantity.textContent = cc
+    tableTotal.textContent = total
+})
+
+ccMinusBtn.addEventListener(`click`,function() {
+    if(cc > 0){
+        cc = cc - 1
+        total = gb + cc + sugar
+        ccQuantity.textContent = cc
+        tableTotal.textContent = total
+    }
+})
+
+sugarPlusBtn.addEventListener('click', function() {
+    sugar = sugar + 1
+    total = gb + cc + sugar
+    sugarQuantity.textContent = sugar
+    tableTotal.textContent = total
+})
+
+sugarMinusBtn.addEventListener(`click`,function() {
+    if(sugar > 0){
+        sugar = sugar - 1
+        total = gb + cc + sugar
+        sugarQuantity.textContent = sugar
+        tableTotal.textContent = total
+    }
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
